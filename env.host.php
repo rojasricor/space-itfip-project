36761a4e06990bb2c90911d32355f6ec9453da93
Ricardo Rojas Rico <rojasricor@gmail.com>
2023-03-16 09:54:42 -0500

    Herramientas modificadas

--- env.host.php
+++
@@ -1,5 +0,0 @@
-<?php exit; ?>
-BASE_DE_DATOS_MYSQL = 'u361342399_spaceitfip_db'
-USUARIO_MYSQL       = 'u361342399_maurengomez'
-CLAVE_MYSQL         = 'Mausuga9712'
-SERVIDOR_MYSQL      = '127.0.0.1:3306'
